eea4f265289e8a4e6d5dd1ca36225165d8fb2bf4
W1nterFr3ak <winterfreak@protonmail.com>
2020-02-11 19:56:24 +0300

    better version

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 pkg  update
 pkg install python3
 pkg install apksigner
-
+pip3 install pyfiglet
 git clone https://github.com/Lexiie/Termux-Apktool
 cd Termux-Apktool
 dpkg -i apktool_2.3.4_all.deb
