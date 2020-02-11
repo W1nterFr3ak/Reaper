6fec8c939289ded02a0a2843b444b885a2747ea2
W1nterFr3ak <winterfreak@protonmail.com>
2020-02-11 19:50:55 +0300

    better version

--- config.sh
+++ config.sh
@@ -1,11 +1,12 @@
 pkg  update
+pkg install python3
 pkg install apksigner
 
 git clone https://github.com/Lexiie/Termux-Apktool
 cd Termux-Apktool
 dpkg -i apktool_2.3.4_all.deb
 cd ..
-echo "alias reaper='python `pwd`/reaper.py'" >> ~/.bashrc
+echo "alias reaper='python3 `pwd`/reaper.py'" >> ~/.bashrc
 
 source ~/.bashrc
 exit
